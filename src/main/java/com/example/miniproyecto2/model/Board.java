5eef63b750990c61cc8483f1787108429e3155f3
Maycol Andres <151478637+Maycol-2018@users.noreply.github.com>
2024-10-23 18:39:14 -0500

    Se elimina la declaración static de los metodos

--- src/main/java/com/example/miniproyecto2/model/Board.java
+++ src/main/java/com/example/miniproyecto2/model/Board.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Board {
+public class Board implements IBoard{
     // Lista que contiene otras listas
     static List<List<Integer>>  matriz = new ArrayList<>();
 
     static boolean[][] rows = new boolean[6][7]; // Para verificar si un número se ha utilizado en una fila
     static boolean[][] columns = new boolean[6][7]; // Para verificar si un número se ha utilizado en una columna
 
-    public static boolean isSafe(int f, int c, int num) {
+    public Board() {}
+
+    public boolean isSafe(int f, int c, int num) {
         return !rows[f][num] && !columns[c][num];
     }
 
     // Verifica que en los cuadrantes los numeros no se repitan
-    public static boolean isBlockSafe(int i, int j, int value){
+    public boolean isBlockSafe(int i, int j, int value){
         int posI = blockRows(i);
         int posJ = blockColumns(j);
 
@@ -31,33 +33,33 @@ public class Board {
     }
 
     // Metodo que retorna la fila del bloque en que se encuentra el valor que se esta probando
-    public static int blockRows(int f){
+    public int blockRows(int f){
         if(f<=1) return 2;
         if(f<=3) return 4;
         else return 6;
     }
 
     // Metodo que retorna la columna del bloque en que se encuentra el valor que se esta probando
-    public static int blockColumns(int c){
+    public int blockColumns(int c){
         if(c<=2) return 3;
         else return 6;
     }
 
-    public static void insertNumber(int f, int c, int num) {
+    public void insertNumber(int f, int c, int num) {
         matriz.get(f).set(c,num);
 
         rows[f][num] = true;
         columns[c][num] = true;
     }
 
-    public static void removeNumber(int f, int c, int num) {
+    public void removeNumber(int f, int c, int num) {
         matriz.get(f).set(c,0);
 
         rows[f][num] = false;
         columns[c][num] = false;
     }
 
-    public static boolean solveSudoku(int f, int c) {
+    public boolean solveSudoku(int f, int c) {
         // Si hemos llegado al final de la matriz, hemos completado el Sudoku
         if (f == 6) {
             return true;
@@ -99,7 +101,7 @@ public class Board {
         return false;
     }
 
-    public static void fillMatriz() {
+    public void fillMatriz() {
 
         // Añadimos las filas (listas internas) a la lista externa
         for (int i = 0; i < 6; i++) {
@@ -111,15 +113,6 @@ public class Board {
         }
 
         solveSudoku(0, 0);
-    }
-
-    public static void showMatriz(){
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(matriz.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
     }
 
     public static List<List<Integer>>  getMatriz() {
