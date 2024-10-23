f7675e05d9fbb08c2096fcc23f9132841c5ae078
Maycol Andres <151478637+Maycol-2018@users.noreply.github.com>
2024-10-23 18:37:00 -0500

    Se actualiza los metodos del IBoard

--- src/main/java/com/example/miniproyecto2/model/IBoard.java
+++ src/main/java/com/example/miniproyecto2/model/IBoard.java
@@ -1,5 +1,12 @@
 package com.example.miniproyecto2.model;
 
 public interface IBoard {
-
+    boolean isSafe(int f, int c, int num);
+    boolean isBlockSafe(int i, int j, int value);
+    int blockRows(int f);
+    int blockColumns(int c);
+    void insertNumber(int f, int c, int num);
+    void removeNumber(int f, int c, int num);
+    boolean solveSudoku(int f, int c);
+    void fillMatriz();
 }
